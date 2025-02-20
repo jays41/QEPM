a0aeb88edcf0bfc44470609d1b6aa7bf0f5727ef
jays41 <jayrs2016@gmail.com>
2025-02-20 12:30:05 +0000

    Add functions to calculate financial metrics: ROTC, Price/EBITDA, and Cash Conversion Cycle

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,33 @@
 import pandas as pd
 
+def get_rotc(data):
+    try:
+        rotc = data['opmad'] / (data['debt_assets'] + (1 - row['debt_assets']))
+    except:
+        print(f'Unable to calculate rotc for {data['ticker']}')
+        rotc = None
+
+    return rotc
+
+def get_price_ebitda(data):
+    try:
+        price_ebitda = data['pe_inc'] * (data['npm'] / data['opmbd'])
+    except:
+        print(f'Unable to calculate price-ebitda for {data['ticker']}')
+        price_ebitda = None
+        
+    return price_ebitda
+
+def get_cash_conversion_cycle(data):
+    try:
+        ccc = (365 / data['inv_turn']) + (365 / data['rect_turn']) - (365 / data['pay_turn'])
+    except:
+        print(f'Unable to calculate cash conversion cycle for {data['ticker']}')
+        ccc = None
+    
+    return ccc
+
+
 df = pd.read_csv('QEPM\data\Data 1500 2010 Start\slmvu0qpmt6cygdi.csv')
 cleaned_df = pd.DataFrame(columns=['gvkey', 'ticker', 'dividend-yield', 'EV-EBITDA', 'price-book', 'price-cf', 'price-earnings', 'price-EBITDA', 'price-sales', 'price-earnings-growth', 'price-earnings-growth-dividend-yield', 'cash-ratio', 'current-ratio', 'quick-ratio', 'inventory-turnover', 'receivables-turnover', 'total-asset-turnover', 'cash-conversion-cycle', 'gross-profit-margin', 'net-profit-margin', 'operating-profit-margin', 'return-on-assets', 'return-on-common-equity', 'return-on-total-capital', 'debt-equity', 'total-debt-ratio', 'interest-coverage-ratio'])
 for i in range(len(df)):
@@ -9,33 +37,31 @@ for i in range(len(df)):
         'gvkey': row['gvkey'],
         'ticker': row['TICKER'],
         'dividend-yield': row['divyield'],
-        'EV-EBITDA': ,
+        'EV-EBITDA': row['evm'], # enterprice value multiple
         'price-book': row['ptb'],
         'price-cf': row['pcf'],
         'price-earnings': row['pe_inc'], # two options for this, was not sure which one to use: "P/E (Diluted, Excl. EI)": "pe_exi", "P/E (Diluted, Incl. EI)": "pe_inc"
-        'price-EBITDA': ,
+        'price-EBITDA': get_price_ebitda(row),
         'price-sales': row['ps'],
-        'price-earnings-growth': ,
-        'price-earnings-growth-dividend-yield': ,
+        'price-earnings-growth': row['peg_ltg_forward'] or row['peg_1yr_forward'] or row['peg_trailing'], # multiple values available - not sure which we want to use
+        'price-earnings-growth-dividend-yield': None, # can do once we choose which peg ratio to use
         'cash-ratio': row['cash_ratio'],
         'current-ratio': row['curr_ratio'],
         'quick-ratio': row['quick_ratio'],
         'inventory-turnover': row['inv_turn'],
         'receivables-turnover': row['rect_turn'],
         'total-asset-turnover': row['at_turn'], # asset turnover
-        'cash-conversion-cycle': ,
+        'cash-conversion-cycle': get_cash_conversion_cycle(row),
         'gross-profit-margin': row['gpm'],
         'net-profit-margin': row['npm'],
-        'operating-profit-margin': ,
+        'operating-profit-margin': row['opmad'] or row['opmbd'], # two options - not sure which to use
         'return-on-assets': row['roa'],
-        'return-on-common-equity': ,
-        'return-on-total-capital': ,
-        'debt-equity': ,
-        'total-debt-ratio': ,
+        'return-on-common-equity': row['roe'], # return on equity
+        'return-on-total-capital': get_rotc(row),
+        'debt-equity': row['de_ratio'],
+        'total-debt-ratio': row['debt_at'],
         'interest-coverage-ratio': row['intcov_ratio']
     })
-
-
 
 '''
 dividend-yield, EV-EBITDA, price-book, price-cf, price-earnings, price-EBITDA, price-sales, price-earnings-growth, price-earnings-growth-dividend-yield, cash-ratio, current-ratio, quick-ratio, inventory-turnover, receivables-turnover, total-asset-turnover, cash-conversion-cycle, gross-profit-margin, net-profit-margin, operating-profit-margin, return-on-assets, return-on-common-equity, return-on-total-capital, debt-equity, total-debt-ratio, interest-coverage-ratio
