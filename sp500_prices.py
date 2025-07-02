09d91670ec901107a40d86105eac1e95166f625e
jays41 <jayrs2016@gmail.com>
2025-07-02 13:59:46 +0100

    Simple script to retrieve s&p500 prices from saved data

---
+++ sp500_prices.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def get_sp500_prices(start_date, end_date):
+    sp500_ticker = "^GSPC"
+    
+    data = pd.read_csv(r"QEPM\data\s&p_data.csv")
+    data['Date'] = pd.to_datetime(data['Date'])
+    filtered_data = data.loc[(data['Date'] >= pd.to_datetime(start_date)) & (data['Date'] <= pd.to_datetime(end_date)), ["Date", "Close"]]
+    return filtered_data
